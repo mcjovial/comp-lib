017413674ffd491495ba79bfc743267e3c826e2b
Emmanuel Elias <emmanuelelias455@gmail.com>
2023-01-11 22:23:29 +0100

    aadded scss to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,10 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/preset-scss"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
 }
